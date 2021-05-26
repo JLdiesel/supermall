fb3f79f7c1781eff9fae46a127030a4e411cb036
JLdiesel <76396097+JLdiesel@users.noreply.github.com>
2021-05-26 21:08:22 +0800

    Add files via upload

---
+++ supermall/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav"></div>
+  <main-tab-bar id="mainTabBar" />
+  <router-view />
+</template>
+<script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+export default {
+  name: "App",
+  components: {
+    MainTabBar,
+  },
+};
+</script>
+<style>
+@import url("assets/css/base.css");
+#mainTabBar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
